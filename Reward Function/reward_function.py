8e96862e24c995373b534989dd8d32746bc22b9f
surajn581 <surajn581@gmail.com>
2023-07-28 01:16:30 +0530

    removed commented code, extra empty lines and spacings.

--- Reward Function/reward_function.py
+++ Reward Function/reward_function.py
@@ -28,34 +28,28 @@ params = {
 """
 
 import math
+import logging
 
 
 def dist(point1, point2):
     return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
-
-
 
 def rect(r, theta):
     """
     theta in degrees
     returns tuple; (float, float); (x,y)
     """
-
     x = r * math.cos(math.radians(theta))
     y = r * math.sin(math.radians(theta))
     return x, y
-
-
 
 def polar(x, y):
     """
     returns r, theta(degrees)
     """
-
     r = (x ** 2 + y ** 2) ** .5
     theta = math.degrees(math.atan2(y,x))
     return r, theta
-
 
 def angle_mod_360(angle):
     """
@@ -66,7 +60,6 @@ def angle_mod_360(angle):
     :param angle: angle in degree
     :return: angle in degree. Between -180 and +180
     """
-
     n = math.floor(angle/360.0)
 
     angle_between_0_and_360 = angle - n*360.0
@@ -76,14 +69,12 @@ def angle_mod_360(angle):
     else:
         return angle_between_0_and_360 - 360
 
-
 def get_waypoints_ordered_in_driving_direction(params):
     # waypoints are always provided in counter clock wise order
     if params['is_reversed']: # driving clock wise.
         return list(reversed(params['waypoints']))
     else: # driving counter clock wise.
         return params['waypoints']
-
 
 def up_sample(waypoints, factor):
     """
@@ -94,10 +85,8 @@ def up_sample(waypoints, factor):
     """
     p = waypoints
     n = len(p)
-
     return [[i / factor * p[(j+1) % n][0] + (1 - i / factor) * p[j][0],
              i / factor * p[(j+1) % n][1] + (1 - i / factor) * p[j][1]] for j in range(n) for i in range(factor)]
-
 
 def get_target_point(params):
 
@@ -107,35 +96,6 @@ def get_target_point(params):
     
     next_closest = closest_waypoint[1]
     return driving_dir_waypoints[next_closest]
-    
-#    index_wp = driving_dir_waypoints.index(next_closest)
-#
-#    input_up_sample = driving_dir_waypoints[index_wp:]
-#    
-#    
-#    waypoints = up_sample(input_up_sample, 1)
-#
-#    car = [params['x'], params['y']]
-
-#    distances = [dist(p, car) for p in waypoints]
-#    min_dist = min(distances)
-#    i_closest = distances.index(min_dist)
-#
-#    n = len(waypoints)
-#
-#    waypoints_starting_with_closest = [waypoints[(i+i_closest) % n] for i in range(n)]
-
-#    r = params['track_width'] * 0.9
-#
-#    is_inside = [dist(p, car) < r for p in waypoints]
-#    i_first_outside = is_inside.index(False)
-
-#    if i_first_outside < 0:  # this can only happen if we choose r as big as the entire track
-#        return waypoints[i_closest]
-
-#    return waypoints_starting_with_closest[i_first_outside]
-
-
 
 def get_target_steering_degree(params):
     tx, ty = get_target_point(params)
@@ -144,13 +104,9 @@ def get_target_steering_degree(params):
     dx = tx-car_x
     dy = ty-car_y
     heading = params['heading']
-
     _, target_angle = polar(dx, dy)
-
     steering_angle = target_angle - heading
-
     return angle_mod_360(steering_angle)
-
 
 def get_progress_score(params, error):
     steps = params['steps']
